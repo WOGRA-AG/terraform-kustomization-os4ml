90c95a00ad2ffb0d9a08663ab8cbe336ab0b4e5f
Thomas Fraunholz <tf@wogra.com>
2022-10-14 10:25:33 +0200

    initial commit

---
+++ argocd.tf
@@ -0,0 +1,37 @@
+data "kustomization_build" "argocd" {
+  path = var.cluster_provisioner == "${path.module}/manifests/argocd/overlays/istio"
+}
+
+resource "kustomization_resource" "argocd_p0" {
+  for_each = data.kustomization_build.argocd.ids_prio[0]
+
+  manifest = (
+    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
+    ? sensitive(data.kustomization_build.argocd.manifests[each.value])
+    : data.kustomization_build.argocd.manifests[each.value]
+  )
+}
+
+resource "kustomization_resource" "argocd_p1" {
+  for_each = data.kustomization_build.argocd.ids_prio[1]
+
+  manifest = (
+    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
+    ? sensitive(data.kustomization_build.argocd.manifests[each.value])
+    : data.kustomization_build.argocd.manifests[each.value]
+  )
+
+  depends_on = [kustomization_resource.argocd_p0]
+}
+
+resource "kustomization_resource" "argocd_p2" {
+  for_each = data.kustomization_build.argocd.ids_prio[2]
+
+  manifest = (
+    contains(["_/Secret"], regex("(?P<group_kind>.*/.*)/.*/.*", each.value)["group_kind"])
+    ? sensitive(data.kustomization_build.argocd.manifests[each.value])
+    : data.kustomization_build.argocd.manifests[each.value]
+  )
+
+  depends_on = [kustomization_resource.argocd_p1]
+}
---
+++ examples/kubernetes/main.tf
@@ -0,0 +1,6 @@
+terraform {
+}
+
+module "os4ml" {
+  source = "WOGRA-AG/os4ml/kustomization"
+}
