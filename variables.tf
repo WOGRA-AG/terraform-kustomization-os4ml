a9dd6ea8070877c16e72ec0accefc81154890d87
Tim Köhler <tk@wogra.com>
2023-03-23 08:17:01 +0100

    remove all locally unnecessary services and configuration

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "kubernetes_config_path" {
   description = "Path of Kubernetes configuration file"
   default     = "~/.kube/config"
 }
+
+variable "deploy_shepard" {
+  description = "Deploy shepard additionally to os4ml"
+  type        = bool
+  default     = false
+}
